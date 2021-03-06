74b2b8131fc53af4adcfb43d402afe1272897d66
Ryan Long <rylong18@gmail.com>
2021-03-06 14:41:21 -0500

    first page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Main from "../components/main/Main";
 
+import { Button } from 'antd';
+
+import './Home.scss';
+
 const Home: React.FC = () => {
+
+  const [step, setStep] = useState(0);
+
+  const nextStep = () => {setStep(step + 1); };
+  const previousStep = () => { setStep(step - 0); };
+  
   return (
-    <Main>
-      <span> testing </span>
+    <Main name='home'>
+      <Button type='primary'> I know what I want to make </Button>
+      <Button type='primary'> Help me find a drink </Button>
+      <Button type='primary'> I'm feeling lucky </Button>
     </Main>
   );
 };
